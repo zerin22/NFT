8a8fe68203df8ecbd9c3293dbab2f6e8ffae45b8
zerin22 <tajrin.ini@gmail.com>
2023-06-14 19:09:19 +0600

    gsap Animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,35 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+
+
+/* GSAP Anumation */
+
+ gsap.from("#banner__animation", {x: '-100%', duration: 4});
+ gsap.fromTo("#bn_des", {opacity: 0}, {opacity: 1, duration: 5});
+
+// var tl = gsap.timeline({repeat: Infinity, repeatDelay: 0});
+var tl = gsap.timeline({repeat: Infinity, duration:1});
+
+tl.fromTo("#how_work", { opacity: 0, scale: 0, rotation:720 }, {  opacity: 1, scale: 1, rotation:0 });
+
+
+tl.to("#how01", {x: 20 });
+tl.to("#how01", {y: 50});
+tl.to("#how01", {opacity: 0});
+
+tl.to("#how02", {x: 20}, '<.5');
+tl.to("#how02", {y: 50}, '<.5');
+tl.to("#how02", {opacity: 0}, '<.5');
+
+tl.to("#how03", {x: 20}, '<.5');
+tl.to("#how03", {y: 50}, '<.5');
+tl.to("#how03", {opacity: 0}, '<.5');
+
+
+
+
+// gsap.to(#, { duration: 2.5, ease: "bounce.out", y: -500 });
+
+/* GSAP Anumation End */
